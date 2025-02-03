109d41089ae426b4e5cd4e0c46b7a34545cd1d7a
Renaud Jenny <renaud.jenny@gmail.com>
2025-02-03 09:17:37 +0100

    Light is now dimmable and color changeable

--- main/esp_zb_light.c
+++ main/esp_zb_light.c
@@ -144,19 +144,56 @@ static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t 
 {
     esp_err_t ret = ESP_OK;
     bool light_state = 0;
-
+    uint8_t light_level = 0;
+    uint16_t light_color_x = 0;
+    uint16_t light_color_y = 0;
     ESP_RETURN_ON_FALSE(message, ESP_FAIL, TAG, "Empty message");
     ESP_RETURN_ON_FALSE(message->info.status == ESP_ZB_ZCL_STATUS_SUCCESS, ESP_ERR_INVALID_ARG, TAG, "Received message: error status(%d)",
                         message->info.status);
     ESP_LOGI(TAG, "Received message: endpoint(%d), cluster(0x%x), attribute(0x%x), data size(%d)", message->info.dst_endpoint, message->info.cluster,
              message->attribute.id, message->attribute.data.size);
     if (message->info.dst_endpoint == BATHROOM_LIGHT_ENDPOINT) {
-        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF) {
+        switch (message->info.cluster) {
+        case ESP_ZB_ZCL_CLUSTER_ID_ON_OFF:
             if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL) {
                 light_state = message->attribute.data.value ? *(bool *)message->attribute.data.value : light_state;
                 ESP_LOGI(TAG, "Light sets to %s", light_state ? "On" : "Off");
                 light_driver_set_power(light_state);
+            } else {
+                ESP_LOGW(TAG, "On/Off cluster data: attribute(0x%x), type(0x%x)", message->attribute.id, message->attribute.data.type);
             }
+            break;
+        case ESP_ZB_ZCL_CLUSTER_ID_COLOR_CONTROL:
+            if (message->attribute.id == ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_X_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_U16) {
+                light_color_x = message->attribute.data.value ? *(uint16_t *)message->attribute.data.value : light_color_x;
+                light_color_y = *(uint16_t *)esp_zb_zcl_get_attribute(message->info.dst_endpoint, message->info.cluster,
+                                                                      ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_Y_ID)
+                                     ->data_p;
+                ESP_LOGI(TAG, "Light color x changes to 0x%x", light_color_x);
+            } else if (message->attribute.id == ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_Y_ID &&
+                       message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_U16) {
+                light_color_y = message->attribute.data.value ? *(uint16_t *)message->attribute.data.value : light_color_y;
+                light_color_x = *(uint16_t *)esp_zb_zcl_get_attribute(message->info.dst_endpoint, message->info.cluster,
+                                                                      ESP_ZB_ZCL_CLUSTER_SERVER_ROLE, ESP_ZB_ZCL_ATTR_COLOR_CONTROL_CURRENT_X_ID)
+                                     ->data_p;
+                ESP_LOGI(TAG, "Light color y changes to 0x%x", light_color_y);
+            } else {
+                ESP_LOGW(TAG, "Color control cluster data: attribute(0x%x), type(0x%x)", message->attribute.id, message->attribute.data.type);
+            }
+            // For some reason, x and y are inverted either in HA or here, but doing so gives better results.
+            light_driver_set_color_xy(light_color_y, light_color_x);
+            break;
+        case ESP_ZB_ZCL_CLUSTER_ID_LEVEL_CONTROL:
+            if (message->attribute.id == ESP_ZB_ZCL_ATTR_LEVEL_CONTROL_CURRENT_LEVEL_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_U8) {
+                light_level = message->attribute.data.value ? *(uint8_t *)message->attribute.data.value : light_level;
+                light_driver_set_level((uint8_t)light_level);
+                ESP_LOGI(TAG, "Light level changes to %d", light_level);
+            } else {
+                ESP_LOGW(TAG, "Level Control cluster data: attribute(0x%x), type(0x%x)", message->attribute.id, message->attribute.data.type);
+            }
+            break;
+        default:
+            ESP_LOGI(TAG, "Message data: cluster(0x%x), attribute(0x%x)  ", message->info.cluster, message->attribute.id);
         }
     }
     return ret;
@@ -185,8 +222,8 @@ static void esp_zb_task(void *pvParameters)
     esp_zb_ep_list_t *ep_list = esp_zb_ep_list_create();
 
     // Light
-    esp_zb_on_off_light_cfg_t light_cfg = ESP_ZB_DEFAULT_ON_OFF_LIGHT_CONFIG();
-    esp_zb_cluster_list_t *light_cluster_list = esp_zb_on_off_light_clusters_create(&light_cfg);
+    esp_zb_color_dimmable_light_cfg_t light_cfg = ESP_ZB_DEFAULT_COLOR_DIMMABLE_LIGHT_CONFIG();
+    esp_zb_cluster_list_t *light_cluster_list = esp_zb_color_dimmable_light_clusters_create(&light_cfg);
     esp_zb_endpoint_config_t light_ep_config = {
         .endpoint = BATHROOM_LIGHT_ENDPOINT,
         .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID,
